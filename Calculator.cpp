6e7e14e9d1fcb8734ec05ce17195325c8096ecb5
Nosherwan <94525538+Nosherwan24925@users.noreply.github.com>
2024-01-24 20:24:12 +0500

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Calculator
+{
+private:
+long number1;
+long number2;
+
+public:
+Calculator();
+long Addition(long x,long y);
+long Subtraction(long x,long y);
+long Multiplication(long x,long y);
+long Division(long x,long y);
+};
+
+
+int main()
+{
+
+return 0;
+}
